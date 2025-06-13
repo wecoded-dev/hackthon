cf5721babfa46c36b418f9f4ff3d3cd839f25a72
KelvinCode1234 <precykelly345@gmail.com>
2025-06-13 13:28:10 +0100

    Feature: polished UI

--- bs-config.js
+++ bs-config.js
@@ -2,12 +2,16 @@ module.exports = {
   server: {
     baseDir: "./",
     middleware: [
-      function (req, next) {
-        const url = req.url;
-        if (!url.includes('.') && url !== '/') {
-          req.url = `${url}.html`;
+      function (req, res, next) {
+        try {
+          const url = req.url;
+          if (!url.includes('.') && url !== '/') {
+            req.url = `${url}.html`;
+          }
+          return next();
+        } catch (err) {
+          return next(err);
         }
-        next();
       }
     ]
   }
